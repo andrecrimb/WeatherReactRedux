a688d35db043bf1d6de2c02b4d328fc087b5061e
Andre Rosa <andre.i.r@live.com>
2018-05-21 00:53:58 -0300

    finishing app

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -2,8 +2,19 @@ import React, {Component} from 'react'
 
 
 class GoogleMap extends Component{
+
+    componentDidMount() {
+        new google.maps.Map(this.refs.map, {
+            zoom: 12,
+            center: {
+                lat: this.props.lat,
+                lng: this.props.lon
+            }
+        })
+    }
+    // isso cria uma referencia para bibliotecas normais que nao conhecem react like #id
     render() {
-        return <div></div>
+        return <div ref="map"></div>
     }
 }
 
